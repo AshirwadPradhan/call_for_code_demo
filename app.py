043d459eab6f2603f5062418c4ff5de4a9752c85
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2018-07-30 21:57:44 +0530

    updated project for bluemix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, jsonify
 from twilio.jwt.access_token import AccessToken
 from twilio.jwt.access_token.grants import VideoGrant
 from faker import Factory
+import os
 
 app = Flask(__name__, static_url_path='')
 fake = Factory.create()
@@ -14,8 +15,8 @@ def index():
 def gen_token():
     # Substitute your Twilio AccountSid and ApiKey details
     ACCOUNT_SID = 'AC26b05c62b2a3faac5bdd3519341fe265'
-    API_KEY_SID = 'SKd2a01ef5ac4f3021fc51dc5ebca65df8'
-    API_KEY_SECRET = 'oQHmHZ1ca9oKW167HZXmXyoMZ3Tyf2Fi'
+    API_KEY_SID = 'SK733ec5148e4224b85c71c9dbba61cf28'
+    API_KEY_SECRET = 'SaHY5Pa52vzWv3iJsi09iylN8URjzgWk'
 
     # Create an Access Token
     token = AccessToken(ACCOUNT_SID, API_KEY_SID, API_KEY_SECRET)
@@ -32,5 +33,7 @@ def gen_token():
     data = jsonify(identity=token.identity, token=token.to_jwt().decode('utf-8'))
     return data
 
+port = os.getenv('VCAP_APP_PORT', '5000')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port=int(port))
